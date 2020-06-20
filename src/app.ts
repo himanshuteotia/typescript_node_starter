8dfa78c6068a132368a2a04a9118f639082bb1c7
himanshuteotia <himanshuteotia7@gmail.com>
2020-06-20 11:38:33 +0530

    app:add logger middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+function loggerMiddleware(
+  request: express.Request,
+  response: express.Response,
+  next
+) {
+  console.log(`${request.method} ${request.path}`);
+  next();
+}
 
 const app = express();
+app.use(loggerMiddleware);
+
 const port = 3000;
 app.get('/', (req, res) => {
   res.send('Ts server is running fine ...');
 });
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
     return console.error(err);
   }
